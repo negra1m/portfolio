214856f8952b61e42b17250452cef80d4a5cdc9a
negra1m <vnsn_@outlook.com>
2022-03-14 03:00:47 -0300

    :constructiom: adding first threejs model into the app

--- src/app/shared/components/animated/animated.component.ts
+++ src/app/shared/components/animated/animated.component.ts
@@ -1,15 +1,109 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, ViewChild, NgZone } from '@angular/core';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 @Component({
   selector: 'app-animated',
   templateUrl: './animated.component.html',
-  styleUrls: ['./animated.component.scss']
+  styleUrls: ['./animated.component.scss'],
 })
-export class AnimatedComponent implements OnInit {
+export class AnimatedComponent implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild('rendererCanvas', {static: true})
+  public rendererCanvas: ElementRef<HTMLCanvasElement>;
+  private canvas: HTMLCanvasElement;
+  private renderer: THREE.WebGLRenderer;
+  private camera: THREE.PerspectiveCamera;
+  private scene: THREE.Scene;
+  private light: THREE.AmbientLight;
+  private loader: GLTFLoader;
+  private spotlight: THREE.SpotLight;
+  private cube: THREE.Mesh;
 
-  ngOnInit(): void {
+  private frameId: number = null;
+
+  constructor(private ngZone: NgZone) {}
+
+  ngAfterViewInit(): void {
+    // this.camera.position.set(-1.8, 0.6, 2.7);
+    this.loadPlanet();
   }
 
+  loadPlanet() {
+
+    this.canvas = this.rendererCanvas.nativeElement;
+
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      alpha: true,    // transparent background
+      antialias: true // smooth edges
+    });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // create the scene
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(
+      75, window.innerWidth / window.innerHeight, 0.1, 1000
+    );
+    this.camera.position.z = 3;
+    this.scene.add(this.camera);
+
+    // soft white light
+    this.light = new THREE.AmbientLight(0x404040);
+    this.light.position.z = 10;
+    const spotLight = new THREE.SpotLight( 0xffffff );
+    spotLight.position.set( 100, 1000, 100 );
+
+    spotLight.castShadow = true;
+
+    spotLight.shadow.mapSize.width = 1024;
+    spotLight.shadow.mapSize.height = 1024;
+
+    spotLight.shadow.camera.near = 500;
+    spotLight.shadow.camera.far = 4000;
+    spotLight.shadow.camera.fov = 30;
+
+    this.scene.add( spotLight );
+    this.scene.add(this.light);
+    const texture = new THREE.TextureLoader().load('../../../../assets/img/earth-texture.jpg');
+    // texture.repeat.set( 4, 4 );
+    const geometry = new THREE.SphereGeometry(0.5, 500, 100, 0.001, 6.28, 0, 3.14);
+    const material = new THREE.MeshBasicMaterial( { map: texture} );
+    this.cube = new THREE.Mesh(geometry, material);
+    // this.cube.rotation.z += 1;
+    this.scene.add(this.cube);
+    this.ngZone.runOutsideAngular(() => {
+      if (document.readyState !== 'loading') {
+        this.render();
+      } else {
+        window.addEventListener('DOMContentLoaded', () => {
+          this.render();
+        });
+      }
+
+      window.addEventListener('resize', () => {
+        this.resize();
+      });
+    });
+  }
+
+  public render(): void {
+    this.frameId = requestAnimationFrame(() => {
+      this.render();
+    });
+
+    this.cube.rotation.x += 0.0000001;
+    this.cube.rotation.y += 0.01;
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  public resize(): void {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+
+    this.renderer.setSize(width, height);
+  }
 }
