c701391408ba862b62421492862ef94119e4d2ee
negra1m <vnsn_@outlook.com>
2022-03-15 00:35:20 -0300

    :art: refactoring

--- src/app/shared/components/animated/animated.component.ts
+++ src/app/shared/components/animated/animated.component.ts
@@ -16,62 +16,47 @@ export class AnimatedComponent implements AfterViewInit {
   private camera: THREE.PerspectiveCamera;
   private scene: THREE.Scene;
   private light: THREE.AmbientLight;
-  private loader: GLTFLoader;
-  private spotlight: THREE.SpotLight;
-  private cube: THREE.Mesh;
+  private planet: THREE.Mesh;
 
   private frameId: number = null;
 
   constructor(private ngZone: NgZone) {}
 
   ngAfterViewInit(): void {
-    // this.camera.position.set(-1.8, 0.6, 2.7);
+    this.setUpThree();
     this.loadPlanet();
   }
 
-  loadPlanet() {
-
+  private setUpThree() {
+    // setup canvas and render
     this.canvas = this.rendererCanvas.nativeElement;
-
     this.renderer = new THREE.WebGLRenderer({
       canvas: this.canvas,
       alpha: true,    // transparent background
       antialias: true // smooth edges
     });
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-
-    // create the scene
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(
       75, window.innerWidth / window.innerHeight, 0.1, 1000
     );
+    this.setUpSizesView(window.innerWidth, window.innerHeight);
+  }
+
+  private setUpSizesView(innerWidth, innerHeight) {
+    this.renderer.setSize(innerWidth, innerHeight);
     this.camera.position.z = 3;
     this.scene.add(this.camera);
+  }
 
-    // soft white light
-    this.light = new THREE.AmbientLight(0x404040);
-    this.light.position.z = 10;
-    const spotLight = new THREE.SpotLight( 0xffffff );
-    spotLight.position.set( 100, 1000, 100 );
-
-    spotLight.castShadow = true;
-
-    spotLight.shadow.mapSize.width = 1024;
-    spotLight.shadow.mapSize.height = 1024;
-
-    spotLight.shadow.camera.near = 500;
-    spotLight.shadow.camera.far = 4000;
-    spotLight.shadow.camera.fov = 30;
-
-    this.scene.add( spotLight );
-    this.scene.add(this.light);
+  loadPlanet() {
+    this.setLights();
     const texture = new THREE.TextureLoader().load('../../../../assets/img/earth-texture.jpg');
-    // texture.repeat.set( 4, 4 );
     const geometry = new THREE.SphereGeometry(0.5, 500, 100, 0.001, 6.28, 0, 3.14);
     const material = new THREE.MeshBasicMaterial( { map: texture} );
-    this.cube = new THREE.Mesh(geometry, material);
-    // this.cube.rotation.z += 1;
-    this.scene.add(this.cube);
+    this.planet = new THREE.Mesh(geometry, material);
+
+    this.scene.add(this.planet);
+
     this.ngZone.runOutsideAngular(() => {
       if (document.readyState !== 'loading') {
         this.render();
@@ -80,11 +65,27 @@ export class AnimatedComponent implements AfterViewInit {
           this.render();
         });
       }
-
       window.addEventListener('resize', () => {
         this.resize();
       });
     });
+  }
+
+  private setLights() {
+    this.light = new THREE.AmbientLight(0x404040);
+    const spotLight = new THREE.SpotLight( 0xffffff );
+    this.light.position.z = 10;
+
+    spotLight.position.set( 100, 1000, 100 );
+    spotLight.castShadow = true;
+    spotLight.shadow.mapSize.width = 1024;
+    spotLight.shadow.mapSize.height = 1024;
+    spotLight.shadow.camera.near = 500;
+    spotLight.shadow.camera.far = 4000;
+    spotLight.shadow.camera.fov = 30;
+
+    this.scene.add( spotLight );
+    this.scene.add(this.light);
   }
 
   public render(): void {
@@ -92,8 +93,8 @@ export class AnimatedComponent implements AfterViewInit {
       this.render();
     });
 
-    this.cube.rotation.x += 0.0000001;
-    this.cube.rotation.y += 0.01;
+    this.planet.rotation.x += 0.0000001;
+    this.planet.rotation.y += 0.01;
     this.renderer.render(this.scene, this.camera);
   }
 
