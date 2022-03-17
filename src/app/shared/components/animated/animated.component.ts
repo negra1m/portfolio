845a829f8f9af0a9ddcd94d364fb76702c27397e
negra1m <vnsn_@outlook.com>
2022-03-17 07:01:20 -0300

    :art: removing junkjs after tests

--- src/app/shared/components/animated/animated.component.ts
+++ src/app/shared/components/animated/animated.component.ts
@@ -4,39 +4,39 @@ import {
   AfterViewInit,
   ViewChild,
   NgZone,
-} from "@angular/core";
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-
-import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
-import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
-import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
+} from '@angular/core';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 @Component({
-  selector: "app-animated",
-  templateUrl: "./animated.component.html",
-  styleUrls: ["./animated.component.scss"],
+  selector: 'app-animated',
+  templateUrl: './animated.component.html',
+  styleUrls: ['./animated.component.scss'],
 })
 export class AnimatedComponent implements AfterViewInit {
-  @ViewChild("rendererCanvas", { static: true })
+  @ViewChild('rendererCanvas', { static: true })
   public rendererCanvas: ElementRef<HTMLCanvasElement>;
   private canvas: HTMLCanvasElement;
   private renderer: THREE.WebGLRenderer;
   private camera: THREE.PerspectiveCamera;
   private scene: THREE.Scene;
   private light: THREE.AmbientLight;
-  planetgeometry: THREE.SphereGeometry;
+  private planetgeometry: THREE.SphereGeometry;
+  private planetsgeometry: THREE.SphereGeometry;
   private planet: THREE.Mesh;
-  private planets: [THREE.Mesh];
-  private axis = new THREE.Vector3(0, 1, 0).normalize();
-  controls;
-  bloomComposer;
-
-  private frameId: number = null;
-
+  private controls;
   constructor(private ngZone: NgZone) {
     this.planetgeometry = new THREE.SphereGeometry(
       0.5,
+      5000,
+      100,
+      0.001,
+      6.28,
+      0,
+      3.14
+    );
+    this.planetsgeometry = new THREE.SphereGeometry(
+      0.3,
       5000,
       100,
       0.001,
@@ -52,6 +52,7 @@ export class AnimatedComponent implements AfterViewInit {
     this.loadPlanet();
     this.loadPlanets();
     this.setupMovement();
+    this.setupControls();
   }
 
   private setUpThree() {
@@ -71,29 +72,8 @@ export class AnimatedComponent implements AfterViewInit {
       35,
       window.innerWidth / window.innerHeight,
       0.1,
-      1000
-    );
-    
+      1000);
     this.setUpSizesView(window.innerWidth, window.innerHeight);
-
-    //tests
-    //bloom renderer
-    const renderScene = new RenderPass( this.scene, this.camera );
-
-				const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
-				bloomPass.threshold = 0.14;
-				bloomPass.strength = 1.93;
-				bloomPass.radius = 0.2;
-
-        this.bloomComposer = new EffectComposer(this.renderer);
-        this.bloomComposer.addPass(renderScene);
-        this.bloomComposer.addPass(bloomPass);
-
-
-    // this.bloomComposer = new EffectComposer(this.renderer);
-    // this.bloomComposer.setSize(window.innerWidth, window.innerHeight);
-    // this.bloomComposer.renderToScreen = true;
-
   }
 
   private setUpSizesView(innerWidth, innerHeight) {
@@ -104,30 +84,30 @@ export class AnimatedComponent implements AfterViewInit {
 
   loadPlanet() {
     const texture = new THREE.TextureLoader().load(
-      "assets/img/earth-texture.jpg"
+      'assets/img/earth-texture.jpg'
     );
     const material = new THREE.MeshPhongMaterial({
       map: texture,
-      bumpMap: new THREE.TextureLoader().load("assets/img/earth-bump.jpg"),
+      bumpMap: new THREE.TextureLoader().load('assets/img/earth-bump.jpg'),
       bumpScale: 0.05,
-      specularMap: new THREE.TextureLoader().load("assets/img/earth-spec.jpg"),
-      specular: new THREE.Color("grey"),
-      alphaMap: new THREE.TextureLoader().load("assets/img/planet.jpg"),
+      specularMap: new THREE.TextureLoader().load('assets/img/earth-spec.jpg'),
+      specular: new THREE.Color('grey'),
+      alphaMap: new THREE.TextureLoader().load('assets/img/planet.jpg'),
     });
     this.planet = new THREE.Mesh(this.planetgeometry, material);
-    this.planet.name = "earth";
+    this.planet.name = 'earth';
 
     this.scene.add(this.planet);
 
     this.ngZone.runOutsideAngular(() => {
-      if (document.readyState !== "loading") {
+      if (document.readyState !== 'loading') {
         this.render();
       } else {
-        window.addEventListener("DOMContentLoaded", () => {
+        window.addEventListener('DOMContentLoaded', () => {
           this.render();
         });
       }
-      window.addEventListener("resize", () => {
+      window.addEventListener('resize', () => {
         this.resize();
       });
     });
@@ -153,17 +133,18 @@ export class AnimatedComponent implements AfterViewInit {
       );
       const material = new THREE.MeshPhongMaterial({
         map: texture,
-        bumpMap: new THREE.TextureLoader().load("assets/img/planet.jpg"),
+        bumpMap: new THREE.TextureLoader().load('assets/img/planet.jpg'),
         bumpScale: 0.05,
-        specular: new THREE.Color("grey"),
-        alphaMap: new THREE.TextureLoader().load("assets/img/planet.jpg"),
+        specular: new THREE.Color('grey'),
+        alphaMap: new THREE.TextureLoader().load('assets/img/planet.jpg'),
       });
-      const planet = new THREE.Mesh(this.planetgeometry, material);
+      const planet = new THREE.Mesh(this.planetsgeometry, material);
       planet.position.x = Math.round(this.generateRandomNumber(-5, 5));
       planet.position.y = Math.round(this.generateRandomNumber(-2, 5));
       planet.position.z = this.generateRandomNumber();
-      planet.name = "planet";
+      planet.name = 'planet';
 
+      // place planets far from earth
       if (
         counter.includes(planet.position.x) ||
         Math.abs(planet.position.x) < 3
@@ -177,14 +158,14 @@ export class AnimatedComponent implements AfterViewInit {
   }
 
   private setLights() {
+    // TODO: solve why galaxy is not appearing
     // galaxy geometry
     const starGeometry = new THREE.SphereGeometry(0.3, 64, 64);
-
     // galaxy material
     const starMaterial = new THREE.MeshBasicMaterial({
-      map: new THREE.TextureLoader().load("../../../../assets/img/galaxy1.png"),
-      side: THREE.BackSide,
-      transparent: true,
+      map: new THREE.TextureLoader().load('../../../../assets/img/galaxy1.png'),
+      side: THREE.DoubleSide,
+      // transparent: true,
     });
     // galaxy mesh
     const starMesh = new THREE.Mesh(starGeometry, starMaterial);
@@ -192,22 +173,9 @@ export class AnimatedComponent implements AfterViewInit {
     this.scene.add(starMesh);
 
     this.light = new THREE.AmbientLight(0xffffff, 0.6);
-    // const spotLight = new THREE.SpotLight(0xffffff, 0.3);
-    // this.light.position.z = 10;
     const hemiLight = new THREE.HemisphereLight(0xffffff, 0x0ffffff, 0.3);
-    // spotLight.position.set(-10000, 9999, 100);
-    // spotLight.castShadow = true;
-    // spotLight.shadow.mapSize.width = 1024;
-    // spotLight.shadow.mapSize.height = 1024;
-    // spotLight.shadow.camera.near = 500;
-    // spotLight.shadow.camera.far = 4000;
-    // spotLight.shadow.camera.fov = 30;
-
     this.scene.add(hemiLight);
     this.scene.add(this.light);
-
-
-    //
   }
 
   public render(): void {
@@ -223,16 +191,7 @@ export class AnimatedComponent implements AfterViewInit {
   }
 
   setupMovement() {
-    // setup camera movement
-    this.controls = new OrbitControls(this.camera, this.canvas);
-    this.controls.enableDamping = true;
-    this.controls.rotateSpeed = 2;
-    this.controls.autoRotate = true;
-    this.controls.autoRotateSpeed = 1;
-    this.controls.maxPolarAngle = Math.PI * 0.5;
-    this.controls.minDistance = 1;
-    this.controls.maxDistance = 10;
-
+    // setup animations movement
     const clock = new THREE.Clock();
     const tick = () => {
       const time = clock.getElapsedTime();
@@ -244,9 +203,19 @@ export class AnimatedComponent implements AfterViewInit {
           element.rotation.y = 0.2 * time;
         }
       });
-      this.bloomComposer.render();
       this.renderer.render(this.scene, this.camera);
     };
     tick();
+  }
+
+  private setupControls() {
+    this.controls = new OrbitControls(this.camera, this.canvas);
+    this.controls.enableDamping = true;
+    this.controls.rotateSpeed = 2;
+    this.controls.autoRotate = true;
+    this.controls.autoRotateSpeed = 1;
+    this.controls.maxPolarAngle = Math.PI * 0.8;
+    this.controls.minDistance = 1;
+    this.controls.maxDistance = 10;
   }
 }
